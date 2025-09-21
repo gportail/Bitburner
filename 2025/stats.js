4f9960d25663e908b239f03d03761dbd56c1cfd9
garfieldfr <gportail@hotmail.com>
2025-09-21 14:32:58 +0200

    nouvelle version de printServersStats  plus simple a enrichir

--- 2025/stats.js
+++ 2025/stats.js
@@ -1,6 +1,64 @@
 import * as ds from "libs/deepscan.js";
 import * as cl from "libs/colors.js";
 import { getScriptsRunning, timeformat, calculGainSeconds, isPurchasedByPlayer } from "libs/lib.js";
+import { clServer } from "libs/classServer";
+
+
+let info;
+
+function buildInfos() {
+  info = new Array();
+  info.push({ title: 'Host                ', dataFormat: '%-20s', fct: function (ns, srv) { return srv.name } });
+  info.push({ title: 'Root ', dataFormat: '%5t', fct: function (ns, srv) { return srv.RootAcces } });
+  info.push({ title: 'SecL ', dataFormat: '%5s', fct: function (ns, srv) { return ns.formatNumber(srv.SecurityLevel, 2) } });
+  info.push({ title: 'minSL', dataFormat: '%5s', fct: function (ns, srv) { return ns.formatNumber(srv.MinSecurityLevel, 0) } });
+  info.push({ title: 'Skill', dataFormat: '%5d', fct: function (ns, srv) { return srv.RequiredHackingLevel } });
+  info.push({ title: 'Port', dataFormat: '%4d', fct: function (ns, srv) { return ns.formatNumber(srv.NumPortsRequired, 0) } });
+  info.push({ title: ' Max$  ', dataFormat: '%7s', fct: function (ns, srv) { return ns.formatNumber(srv.MaxMoney, 2) } });
+  info.push({ title: ' Cur$  ', dataFormat: '%7s', fct: function (ns, srv) { return ns.formatNumber(srv.MoneyAvailable, 2) } });
+  info.push({ title: '  Cur$% ', dataFormat: '%8s', fct: function (ns, srv) { return ns.formatPercent(srv.PercentMoneyAvailable, 2) } });
+  info.push({ title: ' RAM ', dataFormat: '%5s', fct: function (ns, srv) { return ns.formatRam(srv.MaxRam, 0) } });
+  info.push({ title: ' FRam ', dataFormat: '%6s', fct: function (ns, srv) { return ns.formatRam(srv.FreeRam, 1) } });
+  info.push({ title: 'Hack % ', dataFormat: '%7s', fct: function (ns, srv) { return ns.formatPercent(srv.HackChance, 1) } });  // chance de hack
+  info.push({ title: 'Hack $', dataFormat: '%6s', fct: function (ns, srv) { return ns.formatNumber(srv.GainPerThread, 1) } });  // gain par thread
+  info.push({ title: ' Hack$/s ', dataFormat: '%7s/s', fct: function (ns, srv) { return ns.formatNumber(srv.GainPerSecond, 2) } }); // gain $/s
+  info.push({ title: ' WTime', dataFormat: '%6s', fct: function (ns, srv) { return timeformat(srv.WeakenTime) } });  // durée weaken 
+  info.push({ title: ' GTime', dataFormat: '%6s', fct: function (ns, srv) { return timeformat(srv.GrowTime) } });    // durée grow 
+  info.push({ title: ' HTime', dataFormat: '%6s', fct: function (ns, srv) { return timeformat(srv.HackTime) } });    // durée hack
+  info.push({ title: ' FTime', dataFormat: '%6s', fct: function (ns, srv) { return timeformat(srv.CycleTime) } });// durée d'un cycle WGH
+  info.push({ title: 'Scripts', dataFormat: '%s', fct: function (ns, srv) { let res = srv.RunningScripts.join(); return res == "" ? "" : res; } });
+}
+
+function printServersStats2(ns, onlyRoot, withOwned) {
+  let servers = ds.getServers();
+  buildInfos();
+  let header = "    ";
+  for (let inf of info) {
+    header += "|" + inf.title;
+  }
+  ns.tprintf(`${cl.info}%s`, [header]);
+  ns.tprintf(`${cl.info}%s`, ["".padStart(header.length, "=")]);
+  let count = 1;
+  for (let srv of servers) {
+    if (srv == "home") continue;
+    if (isPurchasedByPlayer(srv, ns) && !withOwned) continue;
+    if (srv == "darkweb" && !ns.hasTorRouter()) continue;
+    if (onlyRoot && !ns.hasRootAccess(srv)) continue;
+    let color = cl.whiteOnGreen;
+    if (count % 2 != 0) color = cl.blackOnLightGrey;
+    if (!ns.hasRootAccess(srv)) color = cl.red;
+    let row = "";
+    let oSrv = new clServer(ns, srv);
+    for (let inf of info) {
+      let data = inf.fct(ns, oSrv);
+      if (data == '') data = " ";
+      row += vsprintf('|' + inf.dataFormat, data);
+    }
+    ns.tprintf(`${color}%3d %s`, count, row);
+    count++;
+  }
+}
+
 
 /**
  * Affiche liste des serveurs trié par HackSkill croissant
@@ -30,8 +88,8 @@ function printServersStats(ns, onlyRoot, withOwned) {
       ns.getServerRequiredHackingLevel(srv),
       ns.getServerNumPortsRequired(srv),
       ns.formatNumber(ns.getServerMaxMoney(srv), 2),
-      // ns.formatNumber(ns.getServerMoneyAvailable(srv), 2),
-      ns.getServerMaxMoney(srv)>0 ? ns.formatPercent(ns.getServerMoneyAvailable(srv)/ns.getServerMaxMoney(srv),2) : 0,
+      ns.formatNumber(ns.getServerMoneyAvailable(srv), 2),
+      // ns.getServerMaxMoney(srv)>0 ? ns.formatPercent(ns.getServerMoneyAvailable(srv)/ns.getServerMaxMoney(srv),2) : 0,
       ns.formatRam(ns.getServerMaxRam(srv), 0),
       ns.formatPercent(ns.hackAnalyzeChance(srv), 2), // chance de hack
       ns.formatNumber(ns.hackAnalyze(srv) * ns.getServerMoneyAvailable(srv), 2),  // gain par thread
@@ -113,7 +171,9 @@ export async function main(ns) {
       break;
   }
   //ns.ui.clearTerminal();
-  printServersStats(ns, !params['a'], params['o']);
+  printServersStats2(ns, !params['a'], params['o']);
   ns.tprintf("\n");
   printHackNodeStats(ns);
+  ns.tprintf("\n");
+  // printServersStats2(ns, !params['a'], params['o']);
 }
