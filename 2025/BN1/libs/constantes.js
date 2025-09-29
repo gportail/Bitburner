e279765a0a2b7fb6a778fa1238a1b4202ae7cf42
garfieldfr <gportail@hotmail.com>
2025-09-29 11:17:41 +0200

    fix renomage v5=>BN1

--- 2025/BN1/libs/constantes.js
+++ 2025/BN1/libs/constantes.js
@@ -5,27 +5,27 @@
 export const moneyFactor = 0.8;  // seuil de 80% du max money pour commencer a hack le serveur
 export const secLvlFactor = 1.15; // seuil du niveau de securite de +25% à partir duquel on considere le hack possible
 export const secLvlTolerance = 0.05;  // tolerance sur le 
-export const ScriptBuyProgram = 'v5/buy-programs.js';
+export const ScriptBuyProgram = 'BN1/buy-programs.js';
 // export const ScriptAutoHack = 'autohack.js';
-export const ScriptDeploySingle = 'v5/deploy_single.js';
+export const ScriptDeploySingle = 'BN1/deploy_single.js';
 // export const ScriptSingleWG = 'single_w_and_g4.js';
 // export const ScriptG = 'loop_g3.js';
 // export const ScriptW = 'loop_w3.js';
 // export const ScriptH = 'loop_h3.js';
-export const ScriptSingleW = 'v5/singleW.js';
-export const ScriptSingleG = 'v5/singleG.js';
-export const ScriptSingleH = 'v5/singleH.js';
+export const ScriptSingleW = 'BN1/singleW.js';
+export const ScriptSingleG = 'BN1/singleG.js';
+export const ScriptSingleH = 'BN1/singleH.js';
 
-export const ScriptShare = 'v5/shareRam.js';
-export const ScriptRunShare = 'v5/run_share.js';
+export const ScriptShare = 'BN1/shareRam.js';
+export const ScriptRunShare = 'BN1/run_share.js';
 
 
-export const ScriptLoopSingleW = 'v5/loop_singleW.js';
-export const ScriptLoopSingleG = 'v5/loop_singleG.js';
-export const ScriptLoopSingleH = 'v5/loop_singleH.js';
+export const ScriptLoopSingleW = 'BN1/loop_singleW.js';
+export const ScriptLoopSingleG = 'BN1/loop_singleG.js';
+export const ScriptLoopSingleH = 'BN1/loop_singleH.js';
 
-export const ScriptBasic = 'v5/basic_hack.js';
+export const ScriptBasic = 'BN1/basic_hack.js';
 // export const ScriptKillOwned = 'kill-all-scripts-on-owned.js';
 export const DeployScripts = ['./basic_hack.js', './libs/constantes.js', "./libs/colors.js", "./libs/logs.js", "./singleW.js", 
   "./singleG.js", "./singleH.js", "./loop_singleW.js", "./loop_singleG.js", "./loop_singleH.js"];
-export const Moniteur32Go = 'v5/moniteur_32Go.js';
+export const Moniteur32Go = 'BN1/moniteur_32Go.js';
