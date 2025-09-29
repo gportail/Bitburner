1942f9c5b22e9c6c823540c30103f393fe5247f3
garfieldfr <gportail@hotmail.com>
2025-09-29 11:17:49 +0200

    fix certain alias

--- 2025/BN1/alias.js
+++ 2025/BN1/alias.js
@@ -1,19 +1,19 @@
 import { runCommand } from "./libs/lib.js";
 
 const alias = [
-  "alias co='run v5/connect.js '",
-  "alias s='run v5/stats.js'",
-  "alias sa='cls; run v5/stats.js -a -o'",
-  "alias witch='run witch.js'",
-  "alias so='cls; run v5/stats.js -o'",
+  "alias co='run BN1/connect.js '",
+  "alias s='run BN1/stats.js'",
+  "alias sa='cls; run BN1/stats.js -a -o'",
+  "alias witch='run BN1/witch.js'",
+  "alias so='cls; run BN1/stats.js -o'",
   "alias clearlog='rm -r logs/'",
   "alias -g h='home'",
   "alias -g l='ls'",
   "alias -g nuke='run NUKE.exe'",
   "alias -g brutessh='run BrusteSSH.exe'",
-  "alias -g s3='scan - analyze 3'",
-  "alias -g s5='scan - analyze 5'",
-  "alias -g s10='scan - analyze 10'",
+  "alias -g s3='scan-analyze 3'",
+  "alias -g s5='scan-analyze 5'",
+  "alias -g s10='scan-analyze 10'",
 ]
 /** @param {NS} ns */
 export async function main(ns) {
