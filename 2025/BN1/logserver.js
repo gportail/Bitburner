833e0045e8013a03dacd8ccc7c8c8e62835da5e7
garfieldfr <gportail@hotmail.com>
2025-10-14 12:09:38 +0200

    serveur de logs

--- 2025/BN1/logserver.js
+++ 2025/BN1/logserver.js
@@ -1,15 +1,41 @@
+/**
+ * Serveur de log
+ */
+
+import * as C from "./libs/constantes.js";
+
+
+/**
+ * Sauvegarde des données de statistique
+ * @param {NS} ns 
+ * @param {JSON} datas 
+ */
+function saveStats(ns, datas) {
+  if (!ns.fileExists(C.logStatFile)) {
+    ns.write(C.logStatFile, "script;from;target;hack;grow;weaken\n", 'a');
+  }
+  let ligne = ns.vsprintf("%s;%s;%s;%f;%f;%f", [datas.script, datas.from, datas.datas.target, datas.datas.hack, datas.datas.grow, datas.datas.weaken])
+  ns.write(C.logStatFile, ligne + "\n", 'a');
+}
+
+
 /** @param {NS} ns */
 export async function main(ns) {
 
   let datas;
 
   while (true) {
-    while (ns.peek(1) != "NULL PORT DATA") {
-      datas = JSON.parse(ns.readPort(1));
-      ns.tprint(datas);
-      ns.tprint(datas.log);
-      ns.tprint(datas.info);
+    for (let port of C.logListenPorts) {
+      while (ns.peek(port) != "NULL PORT DATA") {
+        datas = ns.readPort(port);
+        datas = JSON.parse(datas);
+        switch (port) {
+          case C.logStatPort:
+            saveStats(ns, datas);
+            break;
+        }
+      }
+      await ns.sleep(100);
     }
-    await ns.sleep(500);
   }
 } 
