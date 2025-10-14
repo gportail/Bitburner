0a5abd274435c57209d8591a9bfc7803eb3a0de1
garfieldfr <gportail@hotmail.com>
2025-10-14 11:53:22 +0200

    fix 2 alias

--- 2025/BN1/alias.js
+++ 2025/BN1/alias.js
@@ -3,9 +3,9 @@ import { runCommand } from "./libs/lib.js";
 const alias = [
   "alias co='run BN1/connect.js '",
   "alias s='run BN1/stats.js'",
-  "alias sa='cls; run BN1/stats.js -a -o'",
+  "alias sa='run BN1/stats.js -a -o'",
   "alias witch='run BN1/witch.js'",
-  "alias so='cls; run BN1/stats.js -o'",
+  "alias so='run BN1/stats.js -o'",
   "alias clearlog='rm -r logs/'",
   "alias -g h='home'",
   "alias -g l='ls'",
